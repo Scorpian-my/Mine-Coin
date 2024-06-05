bc8c1fa36103a92324d6404c221f606f433ddc3d
Mahyar - Mortezaei <96655803+Scorpian-my@users.noreply.github.com>
2024-06-05 15:59:20 +0330

    Update scorpian-coin.sql

--- scorpian-coin.sql
+++ scorpian-coin.sql
@@ -1,67 +1,33 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 01, 2024 at 05:04 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `scorpian-coin`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
+SET
+  time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
-  `balanse` varchar(255) DEFAULT NULL,
+  `balanse` varchar(255) NOT NULL,
+  `charge` int(255) DEFAULT NULL,
+  `invites` int(255) DEFAULT NULL,
   `id` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
+INSERT INTO
+  `users` (`username`, `balanse`, `charge`, `invites`, `id`)
+VALUES
+  ('Dev_Scorpian', '483', 5000, 0, 1);
 
-INSERT INTO `users` (`username`, `balanse`, `id`) VALUES
-('Mahyar', '303', 1),
-('Dev_Scorpian', '9545955', 2);
+ALTER TABLE
+  `users`
+ADD
+  PRIMARY KEY (`id`);
 
---
--- Indexes for dumped tables
---
+ALTER TABLE
+  `users`
+MODIFY
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 2;
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
