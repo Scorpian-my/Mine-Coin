54671271e4cdee1b5d52f211cd38758505cdbcf5
Mahyar - Mortezaei <96655803+Scorpian-my@users.noreply.github.com>
2024-06-05 19:19:42 +0330

    Delete get.php

--- get.php
+++
@@ -1,6 +0,0 @@
-<?php
-$number = $_POST['number'];
-$username = $_POST["username"];
-$user = str_replace("?user=", "", $username);
-$connect = mysqli_connect("localhost", "root", "", "scorpian-coin");
-mysqli_query($connect, "UPDATE `users` SET `balanse`=balanse+1 WHERE username='$user';");
